821714be12c2f7f97e2e2b69a4ea939109fa286f
Clint Brodar <clintbrodar@Clints-MacBook-Pro.local>
2019-05-05 15:09:26 -0700

    Psuedo code

---
+++ assets/Javascript/app.js
@@ -0,0 +1,23 @@
+$("document").on("click", function () {
+    // Initialize Firebase
+    // Create a variable to reference the database
+    // Capture user inputs and store them into variables
+    // add a time format variable that handles first train time inputs
+    // on click event to search
+    // prevent page from refreshing itself
+    // Capture user inputs and store them in variables
+    // capture "first train time" input
+    // store in time format
+    // display in time format
+    // push data to the database
+    // Firebase watcher .on("child_added"
+    // storing the snapshot.val() in a variable for convenience
+    // initialize variables for calculated display values
+    // minutes away is the difference in time between next arrival time and current time
+    // this diff function takes the current time (moment()) and calculates the diff
+    // btw it and the first train time, formatted using our time format
+    // create a variable to hold the next arrival
+    // populate HTML elements
+    // Handle the errors
+
+})
